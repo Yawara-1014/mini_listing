bc9fd955b7d6646032ff58c7c32d54af92533cc6
Yawara-1014 <urumajin@gmail.comm>
2020-03-08 14:22:19 +0900

    [add]products_controller #create&params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,12 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +24,10 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price, images_attributes: [:src])
   end
 end
