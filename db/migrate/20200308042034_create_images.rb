6d088c0a9c919f9255b6382f783da3fe2424c992
Yawara-1014 <urumajin@gmail.comm>
2020-03-08 13:23:56 +0900

    [add]product&images.rb table

--- db/migrate/20200308042034_create_images.rb
+++ db/migrate/20200308042034_create_images.rb
@@ -1,7 +1,8 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
-
+      t.string :src
+      t.references :product, foreign: true
       t.timestamps
     end
   end
